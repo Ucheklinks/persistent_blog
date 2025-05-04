ad3b4ae87635ebe4336cc7699895061728fe5037
Uchechukwu <unachukwuuche49@gmail.com>
2025-05-04 20:14:19 +0300

    finish header

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import env from "dotenv";
+
+env.config();
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+
+// https://www.awwwards.com/inspiration/blog-page-muse
